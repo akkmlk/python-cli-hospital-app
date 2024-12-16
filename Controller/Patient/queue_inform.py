7e338fadd165ca5812583404143d55d02a78e656
ghani-star <ghaniismail440@gmail.com>
2024-12-16 11:57:21 +0700

    hjhjhh

--- Controller/Patient/queue_inform.py
+++ Controller/Patient/queue_inform.py
@@ -1,8 +1,5 @@
 import csv
 import os
-# from dashboard import menu
-# import sys
-# sys.path.insert(0, 'D:\New-folder\python-cli-hospital-app\Controller\Patient\dashboard.py')?
 
 def queue_reader(filename):
     os.system('cls')
@@ -14,10 +11,6 @@ def queue_reader(filename):
             for item in row:
                 print(item)
                 
-                # import dashboard
-
         os.system('pause')
-        os.system('cls')    
-        # menu()
-
-# queue_reader('Database/queue.csv')
+       
+       
