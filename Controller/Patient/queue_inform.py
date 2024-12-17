23e790038eac4072f1d85620d122716da6335f4b
Ghans* <ghaniismail440@gmail.com>
2024-12-17 10:26:55 +0700

    Update queue_inform.py
    github jancok

--- Controller/Patient/queue_inform.py
+++ Controller/Patient/queue_inform.py
@@ -6,11 +6,15 @@ def queue_reader(filename, patient_data):
     os.system('cls')
     with open(filename, mode='r') as file:
         
-        reader = csv.reader(file)
+        reader = csv.DictReader(file, delimiter= ';')
 
+        print("\n" + "="*164)
+        print(f"{'Id':<5}{'|':<2}{'Nomor Antrian':<14}{'|':<2}{'Id Pasien':<10}{'|':<2}{'Id Dokter':<10}{'|':<2}{'Jenis Pembayaran':<15}{'|':<2}{'Alasan Berkunjung':<19}{'|':<2}{'Deskripsi':<15}{'|':<2}{'Jadwal Periksa':<10}{'|':<2}{'Ruang':<10}{'|':<2}{'Total Harga':<20}{'|':<2}{'Status':<10}|")
+        print("-"*164)
         for row in reader:
-            for item in row:
-                print(item)
+            print(f"{row['id']:<5}{'|':<2}{row['queue_number']:<14}{'|':<2}{row['patient_id']:<10}{'|':<2}{row['doctor_id']:<10}{'|':<2}{row['payment_type']:<16}{'|':<3}{row['reason_visit']:<18}{'|':<2}{row['description']:<15}{'|':<2}{row['schedule_checked']:<14}{'|':<2}{row['room']:<10}{'|':<2}{row['price_total']:<20}{'|':<2}{row['status']:<10}|")
+           
+        print("="*164)
 
         os.system('pause')
         os.system('cls')    
